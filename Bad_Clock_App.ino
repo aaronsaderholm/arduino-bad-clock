b797d644fcd057804d2f17ff037503a8c0b47d54
Aaron Saderholm <a.saderholm@cabem.com>
2016-10-16 16:13:51 -0400

    More stuff.

--- Bad_Clock_App.ino
+++ Bad_Clock_App.ino
@@ -1,29 +1,94 @@
 
 #include "U8glib.h"
 #include "RTClib.h"
-#include <Wire.h>
 #include <Time.h>
-//#include <Timezone.h>
+
+
 
 #define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by Unix time_t as ten ASCII digits
 #define TIME_HEADER  'T'   // Header tag for serial time sync message
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
-RTC_DS1307 rtc;
-U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE);	// I2C / TWI 
 
-void draw(String text, int x, int y) {
-  int str_len = text.length() + 1; 
-  char char_array[str_len];
-  text.toCharArray(char_array, str_len);
-  // graphic commands to redraw the complete screen should be placed here  
-  u8g.setFont(u8g_font_unifont);
-  //u8g.setFont(u8g_font_osb21);
-  u8g.drawStr( x, y, char_array);
+RTC_DS1307 RTC;
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE);	// I2C / TWI 
+long last_millis = millis();
+
+String sync_message = "nothing";
+
+void setup(void) {
+  Serial.begin(115200);   
+  RTC.begin();
+   
+  // Check if the RTC is running.
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running");
+  }
+ 
+  // This section grabs the current datetime and compares it to 
+  // the compilation time.  If necessary, the RTC is updated.
+  DateTime now = RTC.now();
+  DateTime compiled = DateTime(__DATE__, __TIME__);
+  if (now.unixtime() < compiled.unixtime()) {
+    Serial.println("RTC is older than compile time! Updating");
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+   
+  Serial.println("Setup complete.");
 }
 
-String time(RTC_DS1307 rtc) {
-    DateTime now = rtc.now();
+void loop(void) {
+  last_millis = millis();
+  processSyncMessage(); 
+  u8g.firstPage();  
+  do {
+    drawString(time(RTC), 4, 20);
+    drawString(date(RTC), 4, 40);
+    drawString(sync_message, 4, 60);   
+  } while( u8g.nextPage() );
+
+  long diff = elapsed_time();
+  Serial.println(elapsed_time());
+  delay(1000 - diff);  
+}
+
+void processSyncMessage() {
+
+  while(Serial.available()) {
+    char c = Serial.read() ; 
+    Serial.print(c);  
+  }
+
+  
+    // if time sync available from serial port, update time and return true
+  while(Serial.available() >=  TIME_MSG_LEN ){  // time message consists of header & 10 ASCII digits
+    char c = Serial.read() ; 
+    Serial.print(c);  
+    if( c == TIME_HEADER ) {       
+      time_t pctime = 0;
+      for(int i=0; i < TIME_MSG_LEN -1; i++){   
+        c = Serial.read();          
+        if( c >= '0' && c <= '9'){   
+          pctime = (10 * pctime) + (c - '0') ; // convert digits to a number    
+        }
+      }
+
+      Serial.println(pctime);
+      
+
+      sync_message = String(pctime);
+    }    
+  }
+} 
+  
+long elapsed_time() {
+  long current_time = millis();
+  long diff_time = current_time - last_millis;
+  return diff_time;
+}
+  
+String time(RTC_DS1307 RTC) {
+    DateTime now = RTC.now();
     String string = "";
     string += charToString(now.hour());
     string += ':';
@@ -33,8 +98,8 @@ String time(RTC_DS1307 rtc) {
     return string;
 }
 
-String date(RTC_DS1307 rtc) {
-    DateTime now = rtc.now();
+String date(RTC_DS1307 RTC) {
+    DateTime now = RTC.now();
     String string = "";
     string += now.year();
     string += '/';
@@ -51,50 +116,13 @@ String charToString(int integer) {
     return randomString;
 }
 
-
-void setup(void) {
-  Wire.begin();
-  pinMode(8, OUTPUT);
-  //upinMode(buttonPin, INPUT_PULLUP);
-  Wire.beginTransmission(0x68); // address DS3231
-  Wire.write(0x0E); // select register
-  Wire.write(0b00011100); // write register bitmap, bit 7 is /EOSC
-  Wire.endTransmission();
-}
-
-void loop(void) {
-
-    if(Serial.available() ) 
-  {
-    processSyncMessage();
-  }
-  
-  // picture loop
-  u8g.firstPage();  
-  do {
-    draw(time(rtc), 4, 20);
-    draw(date(rtc), 4, 40);
-  } while( u8g.nextPage() );
-
-  delay(1000);
-  
-}
-
-void processSyncMessage() {
-  // if time sync available from serial port, update time and return true
-  while(Serial.available() >=  TIME_MSG_LEN ){  // time message consists of header & 10 ASCII digits
-    char c = Serial.read() ; 
-    Serial.print(c);  
-    if( c == TIME_HEADER ) {       
-      time_t pctime = 0;
-      for(int i=0; i < TIME_MSG_LEN -1; i++){   
-        c = Serial.read();          
-        if( c >= '0' && c <= '9'){   
-          pctime = (10 * pctime) + (c - '0') ; // convert digits to a number    
-        }
-      }   
-      set(pctime);   // Sync Arduino clock to the time received on the serial port
-    }  
-  }
+void drawString(String text, int x, int y) {
+  int str_len = text.length() + 1; 
+  char char_array[str_len];
+  text.toCharArray(char_array, str_len);
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont);
+  //u8g.setFont(u8g_font_osb21);
+  u8g.drawStr( x, y, char_array);
 }
 
